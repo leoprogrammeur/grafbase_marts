859c12b824ed84076418dee1b4b1b3006c18cddd
leoprogrammeur <developpeurclear@gmail.com>
2023-11-02 10:37:21 +0100

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,104 @@
+export const NavLinks = [
+    {href: '/', key: 'Inspiration', text: 'Inspiration'},
+    {href: '/', key: 'Find projects', text: 'Find projects'},
+    {href: '/', key: 'Learn Development', text: 'Learn Development'},
+    {href: '/', key: 'Career Advancement', text: 'Career Advancement'},
+    {href: '/', key: 'Hire Developpers', text: 'Hire Developpers'},
+]
+
+export const categoryFilters = [
+    "Frontent",
+    "Backend",
+    "Full-Stack",
+    "Mobile",
+    "UI/UX",
+    "Game Dev",
+    "DevOps",
+    "Data Science",
+    "Machine Learning",
+    "Data Analyse",
+    "Data Mining",
+    "Dev Game",
+    "Infography",
+    "Picture",
+    "Designer",
+    "Cybersecurity",
+    "Blockchain",
+    "E-commerce",
+    "Chatbots"
+]
+
+export const footLinks = [
+    {
+        title: "Development Resources",
+        links: [
+            "Freelancing",
+            "Development Hiring",
+            "Development Portfolio",
+            "Development Education",
+            "Creative Process",
+            "Development Industry Trends"
+        ]
+    },
+    {
+        title: "Development assets",
+        links: [
+            "Code Marketplace",
+            "GitHub Marketplace",
+            "NPM Registry",
+            "Packagephobia"
+        ]
+    },
+    {
+        title: "Directories",
+        links: [
+            "Development jobs",
+            "Developpers for hire",
+            "Freelance developers for hire",
+            "Tags",
+            "Places"
+        ]
+    },
+    {
+        title: "Company",
+        links: [
+            "About",
+            "Careers",
+            "Support",
+            "Media Kit",
+            "Testimonials",
+            "API",
+            "Terms of Services",
+            "Privacy policy",
+            "Cookie policy"
+        ]
+    },
+    {
+        title: "Brands",
+        links: [
+            "Advertise with us"
+        ]
+    },
+    {
+        title: "Hire developers",
+        links: [
+            'Post a job opening',
+            "Post a freelance project",
+            "Search for developpers"
+        ]
+    },
+    {
+        title: "For developpers",
+        links: [
+            'Go Pro',
+            "Explore development work",
+            "Development blog",
+            "Code podcast",
+            "Open-source projects",
+            "Refer a friend",
+            "Code of conduct"
+        ]
+    }
+]
+
+
